1ca0ce22d031c6290e6224002963fa3240c8f8fd
matheus <mg-tatangelo@hotmail.com.br>
2019-09-20 20:55:47 -0300

    tentando não me matar porque ta muito dificil mexer nesse bootstrap e ele não me deixa mexer.

---
+++ Controllers/OutonoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projetoPW.Controllers
+{
+    public class OutonoController : Controller
+    {
+        public IActionResult Outono()
+        {
+            return View();
+        }
+    }
+}
